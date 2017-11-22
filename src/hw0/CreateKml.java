39ce4d3a32b2ec14218782ba0f2c30cc8a85df48
Omri <OmriiB@Gmail.com>
2017-11-22 15:33:00 +0200

    <commit message>

--- src/hw0/CreateKml.java
+++ src/hw0/CreateKml.java
@@ -11,41 +11,36 @@ import java.util.Date;
 import de.micromata.opengis.kml.v_2_2_0.Document;
 import de.micromata.opengis.kml.v_2_2_0.Kml;
 import de.micromata.opengis.kml.v_2_2_0.TimePrimitive;
+import de.micromata.opengis.kml.v_2_2_0.TimeStamp;
 
 public class CreateKml {
 
 	public static void csvtokml(File csv) throws IOException, ParseException {
 		final Kml kml = new Kml();
 		Document doc = kml.createAndSetDocument();
-		Date TimeDate = null;
 		FileReader fr = new FileReader(csv);
 		@SuppressWarnings("resource")
 		BufferedReader br = new BufferedReader(fr);
 		String str = br.readLine();
 		str = br.readLine();
+		TimeStamp s = new TimeStamp();
 		double lat, lon;
 		while (str != null) {
 			String[] srr = str.split(",");
-			if (srr[1].contains("/")) {
-				TimeDate = new SimpleDateFormat("dd/MM/yyyy hh:mm").parse(srr[1]);
-				
-			} else {
-				TimeDate = new SimpleDateFormat("yyyy-MM-dd hh:mm").parse(srr[1]);
-			}
+			
+			String when=srr[1];
+			s.setWhen(when);
 			lat = Double.parseDouble((srr[2].trim()));
 			lon = Double.parseDouble((srr[3].trim()));
-
 			doc.createAndAddPlacemark().withName(srr[6]).
-			withOpen(Boolean.TRUE).createAndSetPoint()
+			withOpen(Boolean.TRUE).withTimePrimitive(s).createAndSetPoint()
 			.addToCoordinates(lon, lat);
 
 			str = br.readLine();
 
 		}
-		// marshals into file
 		String KmlFile = "C:\\Users\\Omri Baruch\\Desktop\\CSVFile\\KMLFile\\kml";
 		kml.marshal(new File(KmlFile));
 	
-		System.out.println("dssds");
 	}
 }
