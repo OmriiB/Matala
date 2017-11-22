f1a9a2a68119f8ef13564e26a1f3b39e9bbd88bf
Omri <OmriiB@Gmail.com>
2017-11-22 14:41:52 +0200

    <syso>

--- src/hw0/CreateKml.java
+++ src/hw0/CreateKml.java
@@ -4,7 +4,9 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import de.micromata.opengis.kml.v_2_2_0.Document;
 import de.micromata.opengis.kml.v_2_2_0.Kml;
@@ -12,32 +14,38 @@ import de.micromata.opengis.kml.v_2_2_0.TimePrimitive;
 
 public class CreateKml {
 
-	public static void csvtokml(File csv) throws IOException {
+	public static void csvtokml(File csv) throws IOException, ParseException {
 		final Kml kml = new Kml();
-		Document doc =kml.createAndSetDocument();
-
-		FileReader fr=new FileReader(csv);
+		Document doc = kml.createAndSetDocument();
+		Date TimeDate = null;
+		FileReader fr = new FileReader(csv);
 		@SuppressWarnings("resource")
-		BufferedReader br=new BufferedReader(fr);
+		BufferedReader br = new BufferedReader(fr);
 		String str = br.readLine();
 		str = br.readLine();
-		double lat,lon;
-		while(str != null)
-		{
-			String[] srr=str.split(",");
-			lat=Double.parseDouble((srr[2].trim()));
-			lon=Double.parseDouble((srr[3].trim()));
-			doc.createAndAddPlacemark()
-			.withName(srr[6]).withOpen(Boolean.TRUE)
-			.createAndSetPoint().addToCoordinates(lon, lat);
+		double lat, lon;
+		while (str != null) {
+			String[] srr = str.split(",");
+			if (srr[1].contains("/")) {
+				TimeDate = new SimpleDateFormat("dd/MM/yyyy hh:mm").parse(srr[1]);
+				
+			} else {
+				TimeDate = new SimpleDateFormat("yyyy-MM-dd hh:mm").parse(srr[1]);
+			}
+			lat = Double.parseDouble((srr[2].trim()));
+			lon = Double.parseDouble((srr[3].trim()));
 
-	
+			doc.createAndAddPlacemark().withName(srr[6]).
+			withOpen(Boolean.TRUE).createAndSetPoint()
+			.addToCoordinates(lon, lat);
 
-			str=br.readLine();
+			str = br.readLine();
 
 		}
-		//marshals into file
+		// marshals into file
 		String KmlFile = "C:\\Users\\Omri Baruch\\Desktop\\CSVFile\\KMLFile\\kml";
 		kml.marshal(new File(KmlFile));
+	
+		System.out.println("dssds");
 	}
 }
